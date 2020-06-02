ce38efd03d5c76e14708cc21e6e302c8524afb15
yunliang <yunliang@ding.com>
2020-06-02 14:17:45 +0800

    面试相关链接

--- javascript/js1.ts
+++ javascript/js1.ts
@@ -5,3 +5,48 @@
 ['1','2','3'].map(parseInt) // 总结
 
 // 事件队列
+
+//
+/* 实现一个add方法，使计算结果能够满足如下预期： currying
+add(1)(2)(3) = 6;
+add(1, 2, 3)(4) = 10;
+add(1)(2)(3)(4)(5) = 15;
+function add() {
+    // 第一次执行时，定义一个数组专门用来存储所有的参数
+    var _args = Array.prototype.slice.call(arguments);
+    // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+    var _adder = function() {
+        _args.push(...arguments);
+        return _adder;
+    };
+    // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+    _adder.toString = function () {
+        return _args.reduce(function (a, b) {
+            return a + b;
+        });
+    }
+    return _adder;
+}
+
+add(1)(2)(3)                // 6
+add(1, 2, 3)(4)             // 10
+add(1)(2)(3)(4)(5)          // 15
+*/
+// 数组的empty不会参与迭代
+
+// ResizeObserver 检测dom尺寸变化
+/*
+    var ro = new ResizeObserver( entries => {
+    for (let entry of entries) {
+        const cr = entry.contentRect;
+        console.log('Element:', entry.target);
+        console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+        console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+    }
+    });
+
+    // 观察一个或多个元素
+    ro.observe(eleZxx);
+*/
+// js eventLoop https://www.cnblogs.com/goloving/p/12790991.html
+// node eventLoop 
---
+++ start.js
@@ -0,0 +1,9 @@
+// 简历细节 https://mp.weixin.qq.com/s/XVMaO7ErWp_IE466KUkrIA
+// 2万5千字大厂面经 https://juejin.im/post/5ba34e54e51d450e5162789b
+// 高级前端面试题 https://muyiy.cn/blog/
+// 大前端面试库 http://bigerfe.com/
+// 前端面试手册(简历模板) https://www.cxymsg.com/
+
+// 28道JavaScript面试题 https://mp.weixin.qq.com/s?__biz=Mzg2NDAzMjE5NQ==&mid=2247484582&idx=1&sn=1e9b45b250e758039ffe440d2fc2e431&scene=21#wechat_redirect
+// 43道JavaScript面试题 https://mp.weixin.qq.com/s?__biz=Mzg2NDAzMjE5NQ==&mid=2247484556&idx=1&sn=910a75856941ce8daea1d50fdbee2a64&scene=21#wechat_redirect
+// 58道JavaScript面试题 https://mp.weixin.qq.com/s?__biz=Mzg2NDAzMjE5NQ==&mid=2247484382&idx=1&sn=c9a4a465097eb540b3e61a4bbbc8892e&scene=21#wechat_redirect
