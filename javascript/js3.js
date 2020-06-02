3fcea65695b574fdf3d2e12f1bbc17ba1749dabd
yunliang <yunliang@ding.com>
2020-06-02 17:34:36 +0800

    面试

---
+++ javascript/js3.js
@@ -0,0 +1,40 @@
+// 写一个原型链继承的例子
+function Parent() {
+  this.name = 'parent'
+}
+function Children() {
+  this.age = 12
+}
+Children.prototype = new Parent() // 1: 子类的 prototype 指向父类的实例
+console.log(new Children())
+// 问题一: 当原型链中包含引用类型值的原型时,该引用类型值会被所有实例共享
+function Parent() {
+  this.array = [1,2,3,4,5]
+}
+function Children() {
+}
+Children.prototype = new Parent()
+let c1 = new Children()
+let c2 = new Children()
+c1.array.push(6)
+console.log(c1.array, c2.array) // [1,2,3,4,5,6], [1,2,3,4,5,6]
+// 解决这类问题 借用构造函数 + 组合继承
+// 其一, 保证了原型链中引用类型值的独立,不再被所有实例共享;
+// 其二, 子类型创建时也能够向父类型传递参数
+// 使用原型链实现对原型属性和方法的继承,通过借用构造函数来实现对实例属性的继承.
+function Parent() {
+  this.array = [1,2,3,4,5]
+}
+function Children() {
+  Parent.call(this) // 为每个实例 预先构造自己的属性
+}
+Children.prototype = new Parent() // 组合继承
+let c1 = new Children()
+let c2 = new Children()
+c1.array.push(6)
+console.log(c1.array, c2.array) // [1,2,3,4,5,6], [1,2,3,4,5]
+
+// new 操作符 3件事情
+let _obj = {} // 空对象
+_obj.__proto__ == XXClass.prototype
+XXClass.call(_obj) // 调用构造器
--- start.js
+++ start.js
@@ -3,6 +3,8 @@
 // 高级前端面试题 https://muyiy.cn/blog/
 // 大前端面试库 http://bigerfe.com/
 // 前端面试手册(简历模板) https://www.cxymsg.com/
+// 128 面试题 https://juejin.im/post/5eb55ceb6fb9a0436748297d
+// 字节面试题目 https://juejin.im/post/5ed5a40ae51d45789b35aa7e
 
 // 28道JavaScript面试题 https://mp.weixin.qq.com/s?__biz=Mzg2NDAzMjE5NQ==&mid=2247484582&idx=1&sn=1e9b45b250e758039ffe440d2fc2e431&scene=21#wechat_redirect
 // 43道JavaScript面试题 https://mp.weixin.qq.com/s?__biz=Mzg2NDAzMjE5NQ==&mid=2247484556&idx=1&sn=910a75856941ce8daea1d50fdbee2a64&scene=21#wechat_redirect
